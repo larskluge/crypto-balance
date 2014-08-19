bb4634096f77d527b6061bbf4152d66f646afe47
Mariusz Roliński <mariusz@mondoreale.pl>
2014-08-19 23:48:42 +0200

    Simplify

--- bin/balance.js
+++ bin/balance.js
@@ -1,9 +1,3 @@
 #!/usr/bin/env node
-// vim: set ft=js
 
-var path = require('path');
-var fs   = require('fs');
-var lib  = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
-
-require(lib + '/command').run();
-
+require("../lib/command").run();
